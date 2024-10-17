40dcb4fb537bc4028f6b7f5ed31c638dad90ab45
Tiago Kontarski <tiagokontarski@gmail.com>
2024-10-17 03:19:38 -0300

    fix: user seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,11 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => Hash::make('password'),
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//            'password' => Hash::make('password'),
+//        ]);
 
         $this->call(CreateThemesSeeder::class);
     }
